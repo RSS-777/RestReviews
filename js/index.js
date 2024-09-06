64386df75e758869f98189dc4edd7d3206769066
Sergey_js15 <serj.777.front@gmail.com>
2024-09-07 00:11:39 +0300

    Added pages contacts, added navigation, css files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('.burger-menu__input');
+const nav = document.querySelector('.nav');
+
+input.addEventListener('change', () => {
+    if(input.checked) {
+        nav.style.top = '0';
+    } else {
+        nav.style.top = '-100%';
+    }
+})
