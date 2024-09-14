700bed19d0937eb7e0248b18f78c24640c70b4e1
Sergey_js15 <serj.777.front@gmail.com>
2024-09-14 16:24:40 +0300

    hide connection data (for test)

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'serj',
-    password: '2000',
-    database: 'comment_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
